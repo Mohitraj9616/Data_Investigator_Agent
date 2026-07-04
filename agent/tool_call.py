03f293a50043a90c1c2fdfc0924ced88a7288d06
Mohit Rajput <mohitraj16@Mohits-MacBook-Pro.local>
2026-07-04 13:45:45 +0530

    tool call added after testing

--- agent/tool_call.py
+++ agent/tool_call.py
@@ -1,5 +1,9 @@
 import re
-from database_conn_check import run_sql_query,get_schema
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from db_checks.database_conn_check import run_sql_query,get_schema
 
 def extract_table_from_sql(sql:str)->list[str]:
     """
